1ef76165bed00832d188e7a270e3d116a82ce828
george-oliveira <george.moliveir@hotmail.com>
2023-04-13 23:39:10 +0000

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Atualizar o servidor"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 unzip -y
+
+echo "Baixar e copiar arquivos da aplicaçãor"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
